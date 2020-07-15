c1bf8744777461bebd40e5ea646e590110062070
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-15 18:01:10 +0800

    Hello Gatsby! Purple

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello Gatsby!</div>
-}
+export default () => (
+  <div style={{ color: `purple`, fontSize: `72px` }}>Hello Gatsby!</div>
+)
