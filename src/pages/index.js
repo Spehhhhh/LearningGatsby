a55f1fbbfdbf78341cf5106d87e7d79a40b89733
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-15 18:04:14 +0800

    add Unsplash img

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,6 @@ export default () => (
   <div style={{ color: `purple` }}>
     <h1>Hello Gatsby!</h1>
     <p>What a world.</p>
-  {/* highlight-end */}
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
   </div>
 )
