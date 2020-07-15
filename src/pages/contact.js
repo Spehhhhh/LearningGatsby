4d11799b7dd41c16aefd23599833666fdd3b06ab
Alex <30626291+Spehhhhh@users.noreply.github.com>
2020-07-15 18:22:06 +0800

    add contact.js

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from "react"
+import Header from "./components/header"
+import { Link } from "gatsby"
+
+export default () => (
+  <div style={{ color: `teal` }}>
+    <Link to="/">Home</Link>
+    <Header headerText="Contact" />
+    <p>Send us a message!</p>
+  </div>
+)
